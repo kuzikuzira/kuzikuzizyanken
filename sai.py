5e85b926c2c505625ef398296bd347cf1284363e
kuzikuzira <91024278+kuzikuzira@users.noreply.github.com>
2021-09-26 02:43:53 +0900

    Add files via upload

--- sai.py
+++ sai.py
@@ -1,13 +1,7 @@
 ﻿from sys import argv
 
-text=""
-comment=""
-
 import random
 
-num=random.randint(0,5)
-hai=["1","2","3","4","5","6"]
+num=random.randint(1,6)
 
-cpu=hai[num]
-
-print(cpu)
+print(num)
