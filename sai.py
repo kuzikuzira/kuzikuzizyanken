001b525d9fdb089247a21e78c2147d63caa4a021
kuzikuzira <91024278+kuzikuzira@users.noreply.github.com>
2021-09-28 00:14:11 +0900

    Add files via upload

--- sai.py
+++ sai.py
@@ -1,13 +1,21 @@
-from sys import argv
+﻿from sys import argv
 
 import random
+
+text=""
 
 kai=int(argv[1])
 
 num=int(argv[2])
 
-i=0
-while i < kai:
-  dem=random.randint(1,num)
-  i+=1
-  print(dem)
+if kai<=100 and num<=100:
+
+  i=0
+  while i < kai:
+    dem=random.randint(1,num)
+    i+=1
+    text=text+str(dem)
+
+else:
+  text="無理っす。\nできねっす。"
+print(text)
