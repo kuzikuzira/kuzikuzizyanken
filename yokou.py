97071098ea16bb5dad7ab9550e7b28063c431398
kuzikuzira <91024278+kuzikuzira@users.noreply.github.com>
2021-09-30 22:08:59 +0900

    Add files via upload

---
+++ yokou.py
@@ -0,0 +1,13 @@
+﻿import time
+print("時間を入力してして\n")
+
+nyuryoku=input()
+nyuryoku=int(nyuryoku)
+
+for i in range(nyuryoku):
+  time.sleep(1)
+  print(i+1)
+#print("こんにちわ")
+
+#print(nyuryoku)
+#print("さん\n")
